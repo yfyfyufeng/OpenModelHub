4231821d30fa1d0d54e1feb606afcd79b66a7a98
TeenSpirit1107 <mshk2016@icloud.com>
2025-04-26 22:52:52 +0800

    upd agent to accept enum value; disabled output relationship to json

--- database/load_data.py
+++ database/load_data.py
@@ -355,16 +355,19 @@ async def load_json_file(session: AsyncSession, file_path: str, current_user: Us
             data['model_author'] = model_author_recs
             data['model_dataset'] = model_dataset_recs
             # write to file
-            output_path = file_path[:-5]+"_RELATIONSHIP.json"
-            with open(output_path, 'w', encoding='utf-8') as f:
-                json.dump(
-                    data,
-                    f,
-                    ensure_ascii=False,
-                    indent=4,
-                    default=lambda o: o.value if isinstance(o, enum.Enum) else str(o)
-                )
-            print("Relationships written to file:", output_path)
+
+            write_to_file = False
+            if write_to_file:
+                output_path = file_path[:-5]+"_RELATIONSHIP.json"
+                with open(output_path, 'w', encoding='utf-8') as f:
+                    json.dump(
+                        data,
+                        f,
+                        ensure_ascii=False,
+                        indent=4,
+                        default=lambda o: o.value if isinstance(o, enum.Enum) else str(o)
+                    )
+                print("Relationships written to file:", output_path)
 
     print(f"File {file_path} is loaded.")
 
